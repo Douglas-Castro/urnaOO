baa2725680c3d7ab31860804f64d9c72524b1b65
Douglas-Castro <farias.douglascastro@gmail.com>
2021-03-21 17:25:40 -0300

    :bulb: Add main

---
+++ src/principal/Principal.java
@@ -0,0 +1,10 @@
+package principal;
+
+import visao.VisaoCadastroCidade;
+
+public class Principal {
+	public static void main(String[] args) {
+		VisaoCadastroCidade frameCidade = new VisaoCadastroCidade();
+		frameCidade.setVisible(true);
+	}
+}
